28d532e7c05de87680e65f942e0113c439ebe150
Alexander Smirnov <aleksander50.500@gmail.com>
2024-09-29 14:19:16 +0300

    lab1 edit

--- lab1_files/lab1_files.cpp
+++ lab1_files/lab1_files.cpp
@@ -8,124 +8,95 @@
 //Смирнов Александр ПС-21 
 //Компилятор: C++
 //Среда выполнеия: Visual studio 2022
-
 #include <iostream>
 #include <fstream>
 #include <string>
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "RU");
 
     std::string inputFileName, outputFileName;
 
-    std::cout << "Введите название входного файла: ";
+    /*std::cout << "Введите название входного файла: ";
     std::cin >> inputFileName;
     std::cout << "Введите название выходного файла: ";
-    std::cin >> outputFileName;
+    std::cin >> outputFileName;*/
+    inputFileName = "input.txt";
+    outputFileName = "output.txt";
 
     std::ifstream inputFile(inputFileName);
     if (!inputFile.is_open()) {
         std::cerr << "Ошибка! Файл не был открыт." << std::endl;
+        return 1;
     }
 
     std::ofstream outputFile(outputFileName);
     if (!outputFile.is_open()) {
         std::cerr << "Ошибка! Файл не был открыт." << std::endl;
+        return 1;
     }
 
-    std::string str;
-    std::string header;
+    std::string str, header, lastHeader;
     int lastPage = 0;
-    bool isNewParagraph = true; //если есть пустая строка то true, после чтенея заголовка сбрасываю флаг
-    bool isContent = false; //Если ничего не записано то выходной файл удаляется
-
-    //для перой страници и не толлко
-    bool singleLineOnPage = false; //изначально true пока не встретит еще одну строку на cтранице
-    bool isFirstPage = true; //для определения первой стрроки
-    bool onlyOneStrOnFirstPage = false; //если ошибка то фапйл е 
+    bool isNewParagraph = false;
+    bool isFirstParagraph = true;
+    int currentPage = 0;
 
     while (std::getline(inputFile, str)) {
         if (str.empty()) {
-            if (!header.empty()) {
-                if (isFirstPage && singleLineOnPage) {
-                    onlyOneStrOnFirstPage = true;
-                }
-                else {
-                    outputFile << header << " .......... " << lastPage << std::endl;
-                    isContent = true;
-                }
-                header = "";
-                isFirstPage = false;
-            }
             isNewParagraph = true;
-            singleLineOnPage = false;
             continue;
         }
 
         if (str.length() > 2 && str[0] == '-' && str[str.length() - 1] == '-') {
             int pageNumber = 0;
-            for (int i = 1; i < str.length() - 1; i++) {
+            for (int i = 1; i < str.length() - 1; ++i) {
                 if (str[i] < '0' || str[i] > '9') {
-                    std::cout << "Ошибка! Некорректный номер страницы: " << str << std::endl;
+                    std::cerr << "Ошибка! Некорректный номер страницы: " << str << std::endl;
+                    return 1;
                 }
-                pageNumber = pageNumber * 10 + (str[i] - '0'); //строка - строка = число тк в таблице ASCII у них коды сопадают с самим числоом
+                pageNumber = pageNumber * 10 + (str[i] - '0');
             }
 
-            if (lastPage != 0 && pageNumber == lastPage) {
+            if (pageNumber == lastPage) {
                 std::cerr << "Ошибка! Повторяющиеся страницы: " << pageNumber << std::endl;
+                return 1;
             }
-
-            if (lastPage != 0 && pageNumber > lastPage + 1) {
-                std::cerr << "Ошибка! Разрыв между страницами: " << lastPage << " и " << pageNumber << std::endl;
-            }
-
-            if (!header.empty() && !singleLineOnPage) {
-                outputFile << header << " .......... " << lastPage << std::endl;
-                isContent = true;
-                header = "";
+            if (pageNumber > lastPage + 1) {
+                std::cerr << "Ошибка! Пропуск страниц между " << lastPage << " и " << pageNumber << std::endl;
+                return 1;
             }
 
             lastPage = pageNumber;
-            isNewParagraph = true;
-            singleLineOnPage = false;
             continue;
         }
 
-        if (isNewParagraph) {
-            header = str;
+        if (isFirstParagraph) {
+            header = str; 
+            currentPage = lastPage;
+            isFirstParagraph = false;
             isNewParagraph = false;
-            singleLineOnPage = true;
         }
         else {
-            singleLineOnPage = false;
+            if (isNewParagraph) {
+                if (!header.empty()) {
+                    outputFile << header << " .......... " << currentPage << std::endl;
+                }
+                header = str;
+                currentPage = lastPage;
+                isNewParagraph = false;
+            }
+            //else {
+            //    header += " " + str;
+            //}
         }
     }
-    //тк header только передался а строки уже закончились то так
-    //из-за этого если поседняя страице содержит одну строку то она не будет учитываться как продолжение предыдущей страницы
-    if (!header.empty() && lastPage > 0) {
-        if (isFirstPage && singleLineOnPage) {
-            onlyOneStrOnFirstPage = true;
-        }
-        else {
-            outputFile << header << " .......... " << lastPage << std::endl;
-            isContent = true;
-        }
-    }
-
-    if (onlyOneStrOnFirstPage) {
-        std::cerr << "Ошибка! На первой странице только одна строка." << std::endl;
-        outputFile.close(); 
-        std::remove(outputFileName.c_str()); 
-    }
-    else if (isContent) {
-        std::cout << "Оглавление создано" << std::endl;
-    }
-    else {
-        std::cout << "Содержимое файла пусто. Оглавление не создано" << std::endl;
+    if (!header.empty()) {
+        outputFile << header << " .......... " << currentPage << std::endl;
     }
 
     inputFile.close();
     outputFile.close();
+    std::cout << "Оглавление создано" << std::endl;
     return 0;
 }
