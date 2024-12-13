03067a1a9f8a74d295736d791c5e175f36ebb2a4
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2024-12-13 21:35:40 +0300

    edit lab 4

--- lab4_graphs/lab4_graphs.cpp
+++ lab4_graphs/lab4_graphs.cpp
@@ -6,62 +6,63 @@
 //Смирнов Александр ПС-21
 //Язык: C++ 
 //Visual Studio 2022
-
 #include <iostream>
 #include <vector>
 #include <fstream>
 
-const int INF = 1000000000;
+const int maxInt = 1000000000;
 
-struct Edge {
-    int to, weight;
+struct Graph {
+    int to;
+    int weight;
 };
 
-void dijkstraCycle(int start, const std::vector<std::vector<Edge>>& graph) {
+void mainCycle(int start, const std::vector<std::vector<Graph>>& graph) {
     int n = graph.size();
-    int minCycle = INF;
+    int minCycle = maxInt;
 
-    for (int i = 0; i < n; ++i) {
-        std::vector<int> dist(n, INF);
-        dist[start] = 0;
+    for (int i = 0; i < n; i++) {
+        std::vector<int> length(n, maxInt);
+        length[start] = 0;
         std::vector<bool> visited(n, false);
 
-        for (int j = 0; j < n; ++j) {
+        for (int j = 0; j < n; j++) {
             int u = -1;
-            for (int k = 0; k < n; ++k) {
-                if (!visited[k] && (u == -1 || dist[k] < dist[u])) {
+            for (int k = 0; k < n; k++) {
+                if (!visited[k] && (u == -1 || length[k] < length[u])) {
                     u = k;
                 }
             }
 
-            if (u == -1 || dist[u] == INF) break;
+            if (u == -1 || length[u] == maxInt) break;
 
             visited[u] = true;
 
-            for (const Edge& edge : graph[u]) {
-                int v = edge.to;
-                int weight = edge.weight;
+            for (const Graph& Graph : graph[u]) {
+                int v = Graph.to;
+                int weight = Graph.weight;
 
-                if (dist[v] > dist[u] + weight) {
-                    dist[v] = dist[u] + weight;
+                if (length[v] > length[u] + weight) {
+                    length[v] = length[u] + weight;
                 }
             }
         }
 
-        for (const Edge& edge : graph[i]) {
-            if (edge.to == start) {
-                if (dist[i] + edge.weight < minCycle) {
-                    minCycle = dist[i] + edge.weight;
+        for (const Graph& Graph : graph[i]) {
+            if (Graph.to == start) {
+                if (length[i] + Graph.weight < minCycle) {
+                    minCycle = length[i] + Graph.weight;
                 }
 
             }
         }
     }
 
-    if (minCycle == INF) {
-        std::cout << "Цикл не найден из города " << start << "\n";
-    } else {
-        std::cout << "Длина минимального цикла начинается от вершины " << start << " это " << minCycle << "\n";
+    if (minCycle == maxInt) {
+        std::cout << "Цикл не найден из города " << start + 1 << "\n";
+    }
+    else {
+        std::cout << "Длина минимального цикла начинается от города " << start + 1 << " это " << minCycle << "\n";
     }
 }
 
@@ -70,26 +71,35 @@ int main() {
     std::ifstream file("graph.txt");
     if (!file) {
         std::cerr << "Ошибка при открытии файла\n";
-        return 1;
     }
 
     int n, m;
     file >> n >> m;
 
-    std::cout << "Количество вершин: " << n << "\n";
-    std::cout << "Количество рёбер: " << m << "\n";
+    std::cout << "Количество городов: " << n << "\n";
+    std::cout << "Количество дорог: " << m << "\n";
 
-    std::vector<std::vector<Edge>> graph(n);
+    std::vector<std::vector<Graph>> graph(n);
 
     for (int i = 0; i < m; ++i) {
         int u, v, w;
         file >> u >> v >> w;
+
+        graph[u - 1].push_back({ v - 1, w });
+
         std::cout << u << " -> " << v << " вес: " << w << "\n";
-        graph[u].push_back({ v, w });
     }
 
-    int startCity = 1;
-    dijkstraCycle(startCity, graph);
+    int startCity;
+    std::cout << "Введите начальный город (от 1 до " << n << "): ";
+    std::cin >> startCity;
+
+    if (startCity < 1 || startCity > n) {
+        std::cerr << "Ошибка: начальный город вне диапазона!\n";
+    }
+    else {
+        mainCycle(startCity - 1, graph);
+    }
 
     return 0;
 }
